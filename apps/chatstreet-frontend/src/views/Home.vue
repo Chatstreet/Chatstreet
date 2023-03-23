eb6ae47fcf9782a6ba97d1d00c68c1c06cec37cc
loris.30 <loris.syla@edu.tbz.ch>
2023-03-23 14:16:30 +0100

    frontend setup

---
+++ apps/chatstreet-frontend/src/views/Home.vue
@@ -0,0 +1,26 @@
+/* eslint-disable max-len */
+<style lang="scss" src="../styles/views/Home.scss" scoped></style>
+
+<template>
+    <header>
+
+    </header>
+    <h1>Hello and Welcome to Chatstreet</h1>
+    <h3>The safest and best messenger on the markt.
+         The only people on the world who knows about
+         the chat is you and you chatpartner</h3>
+    <footer>
+    <p>Made by David and Loris</p>
+    </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent(
+  {
+    name: 'Home',
+  },
+
+);
+</script>
