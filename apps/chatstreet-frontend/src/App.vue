13c08d7ac208cfef1ffbb16ceab71082fc265640
loris.30 <loris.syla@edu.tbz.ch>
2023-03-23 14:16:30 +0100

    frontend setup

---
+++ apps/chatstreet-frontend/src/App.vue
@@ -0,0 +1,9 @@
+<style lang='scss'>
+@import '@/styles/_global.scss';
+@import '@/styles/_typographie.scss';
+@import '@/styles/_icon-font.scss';
+</style>
+
+<template>
+  <router-view/>
+</template>
