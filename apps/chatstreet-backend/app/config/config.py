2db344ff07ae87c60efd52e45288d5b407861712
DavidAbderhalden <david.abderhalden@hotmail.com>
2023-04-23 00:38:51 +0200

    Correct email url for verification and reset

--- apps/chatstreet-backend/app/config/config.py
+++ apps/chatstreet-backend/app/config/config.py
@@ -11,7 +11,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI: str
     JWT_SECRET_KEY: str = os.environ.get("JWT_SECRET_KEY")
     JWT_TOKEN_LOCATION: list = ['headers', 'cookies']
-    JWT_COOKIE_SECURE: str
+    JWT_COOKIE_SECURE: bool = False
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(weeks=1)
     MAIL_SERVER: str = os.environ.get("MAIL_SERVER")
     MAIL_PORT: int = os.environ.get("MAIL_PORT")
@@ -21,10 +21,12 @@ class Config(object):
     MAIL_USE_SSL: bool = True
     MAIL_DEFAULT_SENDER: str = os.environ.get("EMAIL_SENDER")
     BINARY_FERNET_KEY: str = os.environ.get("BINARY_FERNET_KEY")
+    FRONTEND_BASE_URL: str
 
 
 class ProductionConfig(Config):
     ENV = "production"
+    FRONTEND_BASE_URL: str = os.environ.get("PROD_FRONTEND_BASE_URL")
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://' \
                               f'{os.environ.get("PROD_DATABASE_USERNAME")}:' \
                               f'{os.environ.get("PROD_DATABASE_PASSWORD")}@' \
@@ -33,11 +35,12 @@ class ProductionConfig(Config):
                               f'{os.environ.get("PROD_DATABASE_NAME")}'
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_COOKIE_SECURE = True  # !Important: Requires HTTPS connection
+    JWT_COOKIE_SECURE = False
 
 
 class LocalConfig(Config):
     ENV = "local"
+    FRONTEND_BASE_URL: str = os.environ.get("LOCAL_FRONTEND_BASE_URL")
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://' \
                               f'{os.environ.get("LOCAL_DATABASE_USERNAME")}:' \
                               f'{os.environ.get("LOCAL_DATABASE_PASSWORD")}@' \
@@ -52,6 +55,7 @@ class LocalConfig(Config):
 class DevelopmentConfig(Config):
     ENV = "development"
     TESTING = False
+    FRONTEND_BASE_URL: str = os.environ.get("DEV_FRONTEND_BASE_URL")
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://' \
                               f'{os.environ.get("DEV_DATABASE_USERNAME")}:' \
                               f'{os.environ.get("DEV_DATABASE_PASSWORD")}@' \
