2f4ae235a94e38a55468e3289c527e58f0215832
DavidAbderhalden <david.abderhalden@hotmail.com>
2023-05-09 20:05:15 +0200

    Deployment hotfixes

--- apps/chatstreet-backend/app/mail/methods.py
+++ apps/chatstreet-backend/app/mail/methods.py
@@ -13,15 +13,15 @@ def send(message: Message) -> bool:
         return False
 
 
-def send_verification_mail(recipient: str, verification_code: str) -> bool:
+def send_verification_mail(recipient: str, verification_code: str, username: str, user_tag: int) -> bool:
     from app import conf
     if not is_valid_email(recipient):
         return False
     title: str = 'Verify Your E-Mail'
     template: str = 'mail/verification.html'
-    url: str = f'http://{conf.FRONTEND_BASE_URL}/account/verification/{verification_code}'
+    url: str = f'http://{conf.FRONTEND_BASE_URL}/email-verification/{verification_code}'
     msg = Message(title, recipients=[recipient])
-    msg.html = render_template(template, email=recipient, link=url)
+    msg.html = render_template(template, email=recipient, link=url, user='{username}#{user_tag}'.format(username=username, user_tag=user_tag))
     return send(msg)
 
 
@@ -31,7 +31,7 @@ def send_password_reset_mail(recipient: str, username: str, reset_code: str) -> 
         return False
     title: str = 'Reset Your Password'
     template: str = 'mail/reset_password.html'
-    url: str = f'http://{conf.FRONTEND_BASE_URL}/account/reset/password/{reset_code}'
+    url: str = f'http://{conf.FRONTEND_BASE_URL}/reset-password/{reset_code}'
     msg = Message(title, recipients=[recipient])
     msg.html = render_template(template, username=username, link=url)
     return send(msg)
